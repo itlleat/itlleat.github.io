6692f25160d42f76493ea17f83cf8bb216dbc302
scott schulman <scott.schulman84@gmail.com>
2024-08-09 10:12:26 -0700

    changed a link

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,223 +1,223 @@
-// import React from "react";
-// // import 'normalize.css';
-// import "./Project.css"; // Import the separate CSS file
-// import Card from '@mui/material/Card';
-// import CardContent from '@mui/material/CardContent';
-// // import CardMedia from '@mui/material/CardMedia';
-// import Typography from '@mui/material/Typography';
-// import { CardActionArea, CardActions } from '@mui/material';
+import React from "react";
+// import 'normalize.css';
+import "./Project.css"; // Import the separate CSS file
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+// import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea, CardActions } from '@mui/material';
 
 
 
-// // <Choose isDarkMode={isDarkMode} />
-// // <Stack spacing={2}>
-// // <About isDarkMode={isDarkMode} />
-// // <AboutTwo isDarkMode={isDarkMode} />
-// // <AboutThree isDarkMode={isDarkMode} />
-// // <AboutFour isDarkMode={isDarkMode} />
-// // </Stack> 
+// <Choose isDarkMode={isDarkMode} />
+// <Stack spacing={2}>
+// <About isDarkMode={isDarkMode} />
+// <AboutTwo isDarkMode={isDarkMode} />
+// <AboutThree isDarkMode={isDarkMode} />
+// <AboutFour isDarkMode={isDarkMode} />
+// </Stack> 
 
-// export default function Project({ isDarkMode }) {
-//   const containerClassName = `project-container ${isDarkMode ? 'dark-mode' : 'light-mode'}`;
-//   const textClassName = `project-text ${isDarkMode ? 'dark-text' : 'light-text'}`;
+export default function Project({ isDarkMode }) {
+  const containerClassName = `project-container ${isDarkMode ? 'dark-mode' : 'light-mode'}`;
+  const textClassName = `project-text ${isDarkMode ? 'dark-text' : 'light-text'}`;
 
-//   return (
-//     <div>
-//       <Typography variant="h1" className="project-header">
-//         Server Side Projects
-//       </Typography>
-//       <hr />
+  return (
+    <div>
+      <Typography variant="h1" className="project-header">
+        Server Side Projects
+      </Typography>
+      <hr />
 
-//       <div className={containerClassName}>
-
-
-//         <Card className="project-card">
-//           <CardActionArea>
+      <div className={containerClassName}>
 
 
-//             <a href="https://github.com/itlleat/mongo-mango-social-club">
-//               <div className="card-media">
-//                 <div className="background-image-1">
-//                 </div>
-//               </div>
+        <Card className="project-card">
+          <CardActionArea>
+
+
+            <a href="https://github.com/itlleat/mongo-mango-social-club">
+              <div className="card-media">
+                <div className="background-image-1">
+                </div>
+              </div>
         
-//             <Typography gutterBottom variant="h5" component="div" className="project-link">
-//               NoSQL Social Media API
-//             </Typography>
-//             </a>
-//             <CardContent>
+            <Typography gutterBottom variant="h5" component="div" className="project-link">
+              NoSQL Social Media API
+            </Typography>
+            </a>
+            <CardContent>
          
-//               <Typography variant="body2" className={textClassName}>
-//                 Custom API for a social network that uses Express for routing, a MongoDB database, and the Mongoose ODM.
-//               </Typography>
-//             </CardContent>
-//           </CardActionArea>
-//           <CardActions>
-//             <CardActionArea className="tech-bar">
-//               <a className="repo-link" href="https://github.com/itlleat/mongo-mango-social-club">
-//                 <img className="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt="GitHub" />
-//               </a>
-//               <a href="https://expressjs.com/">
-//                 <img class="grid-item" className="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" alt="Express" />
-//               </a>
-//               <a href="https://www.mongodb.com/">
+              <Typography variant="body2" className={textClassName}>
+                Custom API for a social network that uses Express for routing, a MongoDB database, and the Mongoose ODM.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <CardActionArea className="tech-bar">
+              <a className="repo-link" href="https://github.com/itlleat/mongo-mango-social-club">
+                <img className="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt="GitHub" />
+              </a>
+              <a href="https://expressjs.com/">
+                <img class="grid-item" className="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" alt="Express" />
+              </a>
+              <a href="https://www.mongodb.com/">
 
-//                 <img class="grid-item" className="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-plain-wordmark.svg" alt="MongoDb" />
+                <img class="grid-item" className="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-plain-wordmark.svg" alt="MongoDb" />
 
-//               </a>
-//               <a href="https://nodejs.org/en">
+              </a>
+              <a href="https://nodejs.org/en">
 
-// <img class="grid-item" className="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" alt="NodeJs" />
+<img class="grid-item" className="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" alt="NodeJs" />
 
-// </a>
-// <a href="https://www.javascript.com/">
+</a>
+<a href="https://www.javascript.com/">
 
-// <img class="grid-item" className="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg"  alt="JavaScript" />
+<img class="grid-item" className="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg"  alt="JavaScript" />
 
-// </a>
+</a>
 
-//             </CardActionArea>
-//           </CardActions>
-//         </Card>
-
-
+            </CardActionArea>
+          </CardActions>
+        </Card>
 
 
-//         <Card className="project-card">
-//           <CardActionArea>
-//             <a href="https://text-cairn.herokuapp.com/">
-//               <div className="card-media">
-//                 <div className="background-image-2">
-//                 </div>
-//               </div>
+
+
+        <Card className="project-card">
+          <CardActionArea>
+            <a href="https://github.com/itlleat/text-cairn">
+              <div className="card-media">
+                <div className="background-image-2">
+                </div>
+              </div>
           
            
-//               <Typography gutterBottom variant="h5" component="div" className="project-link">
-//                 PWA Text Editor
-//               </Typography>
-//               </a>
-//               <CardContent>
-//               <Typography variant="body2" className={textClassName}>
-//                 Created with the indexedDB API, this app uses data persistence techniques that create redundancy for browser support and the ability for it to be downloaded and used offline.
-//               </Typography>
-//             </CardContent>
-//           </CardActionArea>
-//           <CardActions>
-//             <CardActionArea className="tech-bar">
-//               <a className="repo-link" href="https://github.com/itlleat/text-cairn">
-//                 <img className="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt="GitHub" />
-//               </a>
-//               <a href="https://expressjs.com/">
-//                 <img class="grid-item" className="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" alt="Express" />
-//               </a>
-//               <a href="https://nodejs.org/en">
+              <Typography gutterBottom variant="h5" component="div" className="project-link">
+                PWA Text Editor
+              </Typography>
+              </a>
+              <CardContent>
+              <Typography variant="body2" className={textClassName}>
+                Created with the indexedDB API, this app uses data persistence techniques that create redundancy for browser support and the ability for it to be downloaded and used offline.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <CardActionArea className="tech-bar">
+              <a className="repo-link" href="https://github.com/itlleat/text-cairn">
+                <img className="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt="GitHub" />
+              </a>
+              <a href="https://expressjs.com/">
+                <img class="grid-item" className="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" alt="Express" />
+              </a>
+              <a href="https://nodejs.org/en">
 
-// <img class="grid-item" className="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" alt="NodeJs" />
+<img class="grid-item" className="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" alt="NodeJs" />
 
-// </a>
-// <a href="https://www.javascript.com/">
+</a>
+<a href="https://www.javascript.com/">
 
-// <img class="grid-item" className="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg"  alt="JavaScript" />
+<img class="grid-item" className="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg"  alt="JavaScript" />
 
-// </a>
-//             </CardActionArea>
-//           </CardActions>
-//         </Card>
+</a>
+            </CardActionArea>
+          </CardActions>
+        </Card>
 
-//         <Card className="project-card">
-//           <CardActionArea>
-//             <a href="https://github.com/itlleat/ethical-capitalism-simulator">
-//               <div className="card-media">
-//                 <div className="background-image-3">
-//                 </div>
-//               </div>
+        <Card className="project-card">
+          <CardActionArea>
+            <a href="https://github.com/itlleat/ethical-capitalism-simulator">
+              <div className="card-media">
+                <div className="background-image-3">
+                </div>
+              </div>
          
           
-//               <Typography gutterBottom variant="h5" component="div" className="project-link">
-//                 E-Commerce Back End
-//               </Typography>
-//               </a>
-//               <CardContent>
-//               <Typography variant="body2" className={textClassName}>
-//                 ORM back-end for internet retail using Express.js API routing to use Sequelize to interact with a MySQL database.
-//               </Typography>
-//             </CardContent>
-//           </CardActionArea>
-//           <CardActions>
-//             <CardActionArea className="tech-bar">
-//               <a className="repo-link" href="https://github.com/itlleat/ethical-capitalism-simulator">
-//                 <img className="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt="GitHub" />
-//               </a>
-//               <a href="https://expressjs.com/">
-//                     <img class="grid-item" className="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" alt="Express" />
-//                 </a>
-//                 <a href="https://nodejs.org/en">
+              <Typography gutterBottom variant="h5" component="div" className="project-link">
+                E-Commerce Back End
+              </Typography>
+              </a>
+              <CardContent>
+              <Typography variant="body2" className={textClassName}>
+                ORM back-end for internet retail using Express.js API routing to use Sequelize to interact with a MySQL database.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <CardActionArea className="tech-bar">
+              <a className="repo-link" href="https://github.com/itlleat/ethical-capitalism-simulator">
+                <img className="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt="GitHub" />
+              </a>
+              <a href="https://expressjs.com/">
+                    <img class="grid-item" className="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" alt="Express" />
+                </a>
+                <a href="https://nodejs.org/en">
 
-// <img class="grid-item" className="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" alt="NodeJs" />
+<img class="grid-item" className="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" alt="NodeJs" />
 
-// </a>
-// <a href="https://aws.amazon.com/what-is/sql/#:~:text=Structured%20query%20language%20(SQL)%20is,relationships%20between%20the%20data%20values.https://aws.amazon.com/what-is/sql/#:~:text=Structured%20query%20language%20(SQL)%20is,relationships%20between%20the%20data%20values.">
+</a>
+<a href="https://aws.amazon.com/what-is/sql/#:~:text=Structured%20query%20language%20(SQL)%20is,relationships%20between%20the%20data%20values.https://aws.amazon.com/what-is/sql/#:~:text=Structured%20query%20language%20(SQL)%20is,relationships%20between%20the%20data%20values.">
 
-// <img class="grid-item" className="logo"  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-plain-wordmark.svg" alt="MySQL" />
+<img class="grid-item" className="logo"  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-plain-wordmark.svg" alt="MySQL" />
 
-// </a>
-// <a href="https://www.javascript.com/">
+</a>
+<a href="https://www.javascript.com/">
 
-// <img class="grid-item" className="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg"  alt="JavaScript" />
+<img class="grid-item" className="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg"  alt="JavaScript" />
 
-// </a>
-//             </CardActionArea>
-//           </CardActions>
-//         </Card>
+</a>
+            </CardActionArea>
+          </CardActions>
+        </Card>
 
-//         <Card className="project-card">
-//           <CardActionArea>
-//             <a href="https://github.com/itlleat/biz-bank/tree/main">
-//               <div className="card-media">
-//                 <div className="background-image-4">
-//                 </div>
-//               </div>
+        <Card className="project-card">
+          <CardActionArea>
+            <a href="https://github.com/itlleat/biz-bank/tree/main">
+              <div className="card-media">
+                <div className="background-image-4">
+                </div>
+              </div>
           
           
-//               <Typography gutterBottom variant="h5" component="div" className="project-link">
-//                 CMS Employee Tracker
-//               </Typography>
-//               </a>
-//               <CardContent>
-//               <Typography variant="body2" className={textClassName}>
-//                 CMS application designed to help non-developers interact with and view information stored in databases using Node.js, Inquirer, and MySQL.
-//               </Typography>
-//             </CardContent>
-//           </CardActionArea>
-//           <CardActions>
-//             <CardActionArea className="tech-bar">
-//               <a className="repo-link" href="https://github.com/itlleat/biz-bank/tree/main">
-//                 <img className="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt="GitHub" />
-//               </a>
-//               <a href="https://nodejs.org/en">
+              <Typography gutterBottom variant="h5" component="div" className="project-link">
+                CMS Employee Tracker
+              </Typography>
+              </a>
+              <CardContent>
+              <Typography variant="body2" className={textClassName}>
+                CMS application designed to help non-developers interact with and view information stored in databases using Node.js, Inquirer, and MySQL.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <CardActionArea className="tech-bar">
+              <a className="repo-link" href="https://github.com/itlleat/biz-bank/tree/main">
+                <img className="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt="GitHub" />
+              </a>
+              <a href="https://nodejs.org/en">
 
-// <img class="grid-item" className="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" alt="NodeJs" />
+<img class="grid-item" className="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" alt="NodeJs" />
 
-// </a>
-// <a href="https://aws.amazon.com/what-is/sql/#:~:text=Structured%20query%20language%20(SQL)%20is,relationships%20between%20the%20data%20values.https://aws.amazon.com/what-is/sql/#:~:text=Structured%20query%20language%20(SQL)%20is,relationships%20between%20the%20data%20values.">
+</a>
+<a href="https://aws.amazon.com/what-is/sql/#:~:text=Structured%20query%20language%20(SQL)%20is,relationships%20between%20the%20data%20values.https://aws.amazon.com/what-is/sql/#:~:text=Structured%20query%20language%20(SQL)%20is,relationships%20between%20the%20data%20values.">
 
-// <img class="grid-item" className="logo"  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-plain-wordmark.svg" alt="MySQL" />
+<img class="grid-item" className="logo"  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-plain-wordmark.svg" alt="MySQL" />
 
-// </a>
-// <a href="https://www.javascript.com/">
+</a>
+<a href="https://www.javascript.com/">
 
-// <img class="grid-item" className="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg"  alt="JavaScript" />
+<img class="grid-item" className="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg"  alt="JavaScript" />
 
-// </a>
+</a>
 
-//             </CardActionArea>
-//           </CardActions>
-//         </Card>
+            </CardActionArea>
+          </CardActions>
+        </Card>
 
-//       </div>
-//     </div>
-//   );
-// }
+      </div>
+    </div>
+  );
+}
 
 
 
